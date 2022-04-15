6ff2b45e87297a77cadfae3f4d085691da581be3
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-04-15 18:28:33 +0000

    1

--- destructuring.js
+++ destructuring.js
@@ -3,10 +3,21 @@
  * command node destructuring.js in the terminal
  */
 
+
 // Destructuring arrays
+let ages = [30, 26, 27];
+let mike = ages[0];
+let jack = ages[1];
+let derick = ages[2];
+console.log(mike, jack, derick);
+
+
+
 
 // Destructuring objects
 
+
 // Destructuring subsets
+
 
 // Using rest parameter syntax
